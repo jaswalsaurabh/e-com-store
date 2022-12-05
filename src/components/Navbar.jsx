de3fdee14fb307b2ab754ca9be9ee3e02c34b708
saurabh-jaswal <jaswalsaurabhjnv@gmail.com>
2022-12-05 22:15:41 +0530

    some more changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import Logo from "../favicon.svg";
 
 const Navbar = () => {
   const state = useSelector((state) => state.cart);
